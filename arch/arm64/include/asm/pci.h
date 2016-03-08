12e1d51299e43fb26c4f8e4482f694ba42fb2126
Jerin Jacob <jerin.jacob at caviumnetworks.com>
2016-03-08 15:52:55 -0500

    arm64: pci: add support for pci_mmap_page_range
    certain X11 servers and user space network drivers frameworks
    need PCI mmaped /sys/bus/pci/devices/B:D:F/resourceX file to access PCI bar
    address space from user space
    
    Signed-off-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>
    Patch-mainline: https://patchwork.kernel.org/patch/6857511/
    [okaya@codeaurora.org: compilation fixes for 4.5 kernel]
    
    Change-Id: I7c5665f2eb665c98bce11e12b3c3cf09c626e95b
    Signed-off-by: Sinan Kaya <okaya@codeaurora.org>

--- arch/arm64/include/asm/pci.h
+++ arch/arm64/include/asm/pci.h
@@ -35,6 +35,12 @@ static inline int pci_proc_domain(struct pci_bus *bus)
 {
 	return 1;
 }
+
+#define HAVE_PCI_MMAP
+extern int pci_mmap_page_range(struct pci_dev *dev, struct vm_area_struct *vma,
+			       enum pci_mmap_state mmap_state,
+			       int write_combine);
+
 #endif  /* CONFIG_PCI */
 
 #endif  /* __KERNEL__ */
